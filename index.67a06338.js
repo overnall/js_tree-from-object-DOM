3d61647ec06c0d438bb16829761ac6c72baaf680
overnall <v.h.olexander@gmail.com>
2024-07-24 10:18:36 +0300

    make build

---
+++ index.67a06338.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.appendChild(r),t){var l=document.createElement("li");l.textContent=a,r.appendChild(l),0!==Object.keys(t[a]).length&&e(l,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.67a06338.js.map
